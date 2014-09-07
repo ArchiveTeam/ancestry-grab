c2554efe756f0774b0d739cddf0579669565d355
Arkiver2 <Arkiver@hotmail.com>
2014-09-07 22:06:03 +0200

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -177,7 +177,7 @@ class WgetArgs(object):
             "--no-parent",
             "--page-requisites",
             "--timeout", "30",
-            "--load-cookies", "cookies.txt"
+#            "--load-cookies", "cookies.txt"
             "--tries", "inf",
             "--span-hosts",
             "--waitretry", "30",
