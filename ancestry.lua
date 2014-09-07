a859447ef3f69ec89fd1067d0ba5fe718b4f9794
Arkiver2 <Arkiver@hotmail.com>
2014-09-07 22:24:06 +0200

    ancestry.lua: retry status code 0 5 times

--- ancestry.lua
+++ ancestry.lua
@@ -359,7 +359,20 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
       return wget.actions.CONTINUE
     end
   elseif status_code == 0 then
-    return wget.actions.ABORT
+    io.stdout:write("\nServer returned "..http_stat.statcode..". Sleeping.\n")
+    io.stdout:flush()
+
+    os.execute("sleep 10")
+
+    tries = tries + 1
+
+    if tries >= 5 then
+      io.stdout:write("\nI give up...\n")
+      io.stdout:flush()
+      return wget.actions.ABORT
+    else
+      return wget.actions.CONTINUE
+    end
   else
     return wget.actions.NOTHING
   end
