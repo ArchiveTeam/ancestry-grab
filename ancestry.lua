a00fec6f5a28aa0de2a5c6dba273446022ea69dc
Arkiver2 <Arkiver@hotmail.com>
2014-09-21 01:08:50 +0200

    ancestry.lua: retry 20 times on error status codes

--- ancestry.lua
+++ ancestry.lua
@@ -654,7 +654,7 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
 
     tries = tries + 1
 
-    if tries >= 10 then
+    if tries >= 20 then
       io.stdout:write("\nI give up...\n")
       io.stdout:flush()
       return wget.actions.ABORT
