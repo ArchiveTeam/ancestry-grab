d5ac5083edb3520297ac95ef234f49d706656d91
Arkiver2 <Arkiver@hotmail.com>
2014-10-30 21:01:55 +0100

    ancestry.lua: fix to not download duplicates

--- ancestry.lua
+++ ancestry.lua
@@ -116,7 +116,7 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
       or string.match(url, "%.js")
       or html == 0 
       or string.match(url, "service%.ancestry%.com") then
-      return true
+      return verdict
     else
       return false
     end
